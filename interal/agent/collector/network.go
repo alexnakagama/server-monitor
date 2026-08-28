4ed25763390bea1d04ac17374d45c1bd57891be1
alexnakagama <alexnakagama4@gmail.com>
2026-08-28 14:22:25 -0300

    finished helper function to read cpu stats

--- interal/agent/collector/network.go
+++ interal/agent/collector/network.go
@@ -1,5 +1,13 @@
 package collector
 
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type cpuStats struct {
 	user    uint64
 	nice    uint64
@@ -11,6 +19,75 @@ type cpuStats struct {
 	steal   uint64
 }
 
-func readCPUStats() (cpuStats, error) {}
+func readCPUStats() (cpuStats, error) {
+	file, err := os.Open("proc/stat")
+	if err != nil {
+		return cpuStats{}, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	if !scanner.Scan() {
+		return cpuStats{}, nil
+	}
+
+	fields := strings.Fields(scanner.Text())
+
+	if len(fields) < 9 {
+		return cpuStats{}, errors.New("invalid proc/stat format")
+	}
+
+	user, err := strconv.ParseUint(fields[1], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	nice, err := strconv.ParseUint(fields[2], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	system, err := strconv.ParseUint(fields[3], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	idle, err := strconv.ParseUint(fields[4], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	iowait, err := strconv.ParseUint(fields[5], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	irq, err := strconv.ParseUint(fields[6], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	softirq, err := strconv.ParseUint(fields[7], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	steal, err := strconv.ParseUint(fields[8], 10, 64)
+	if err != nil {
+		return cpuStats{}, err
+	}
+
+	return cpuStats{
+		user:    user,
+		nice:    nice,
+		system:  system,
+		idle:    idle,
+		iowait:  iowait,
+		irq:     irq,
+		softirq: softirq,
+		steal:   steal,
+	}, nil
+}
 
 func CPUUsage() (float64, error) {}
