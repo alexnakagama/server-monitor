7e899da5aee055b935a69aa3c85b5466e849a26d
alexnakagama <alexnakagama4@gmail.com>
2026-08-28 14:13:31 -0300

    created struct to reprenset the cpu stats

--- interal/agent/collector/network.go
+++ interal/agent/collector/network.go
@@ -1,3 +1,14 @@
 package collector
 
+type cpuStats struct {
+	user    uint64
+	nice    uint64
+	system  uint64
+	idle    uint64
+	iowait  uint64
+	irq     uint64
+	softirq uint64
+	steal   uint64
+}
+
 func CPUUsage() (float64, error) {}
