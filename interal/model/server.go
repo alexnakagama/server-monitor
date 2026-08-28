6dc3e4b8bbaba9708b89fa2ec0253f3d52f01502
alexnakagama <alexnakagama4@gmail.com>
2026-08-28 13:56:11 -0300

    added server model

---
+++ interal/model/server.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type Server struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Hostname  string    `json:"hostname"`
+	OS        string    `json:"os"`
+	CreatedAt time.Time `json:"created_at"`
+}
