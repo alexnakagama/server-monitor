140504f0ed6706e915a94b1d3c17d3b412951ca2
alexnakagama <alexnakagama4@gmail.com>
2026-08-28 14:02:47 -0300

    finished user model

--- interal/model/user.go
+++ interal/model/user.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID           int       `json:"id"`
-	Username 	 string    `json:"username"`
+	Username     string    `json:"username"`
 	Email        string    `json:"email"`
 	PasswordHash string    `json:"-"`
 	CreatedAt    time.Time `json:"created_at"`
