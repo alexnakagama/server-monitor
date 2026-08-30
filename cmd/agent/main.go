cb803ef5e0f3759e57669a212ac1a0e96abd74bf
alexnakagama <alexnakagama4@gmail.com>
2026-08-30 18:38:57 -0300

    created main to test the agents

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/alexnakagama/server-monitor/internal/agent/collector"
+)
+
+func main() {
+	cpu, err := collector.CPUUsage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	memory, err := collector.MemoryUsage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("CPU: %.2f%%\n", cpu)
+	fmt.Println("Memory: %.2f%%\n", memory)
+}
