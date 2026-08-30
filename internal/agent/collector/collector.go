081c2a1ad7e697a28d2c293477d882cb030fae24
alexnakagama <alexnakagama4@gmail.com>
2026-08-30 19:20:03 -0300

    finished funcction to collect metrics

--- internal/agent/collector/collector.go
+++ internal/agent/collector/collector.go
@@ -1,5 +1,38 @@
 package collector
 
-import "github.com/alexnakagama/server-monitor/internal/model"
+import (
+	"time"
 
-func CollectMetrics() (model.Metric, error)
+	"github.com/alexnakagama/server-monitor/internal/model"
+)
+
+func CollectMetrics() (model.Metric, error) {
+	cpu, err := CPUUsage()
+	if err != nil {
+		return model.Metric{}, err
+	}
+
+	memory, err := MemoryUsage()
+	if err != nil {
+		return model.Metric{}, err
+	}
+
+	disk, err := DiskUsage()
+	if err != nil {
+		return model.Metric{}, err
+	}
+
+	network, err := NetworkUsage()
+	if err != nil {
+		return model.Metric{}, err
+	}
+
+	return model.Metric{
+		CPUUsage:       cpu,
+		MemoryUsage:    memory,
+		DiskUsage:      disk,
+		NetworkReceive: network.Received,
+		NetworkSent:    network.Sent,
+		Timestamp:      time.Now(),
+	}, nil
+}
