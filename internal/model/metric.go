8964b11cd9110668e7935f7764924a7b4bfaf8e0
alexnakagama <alexnakagama4@gmail.com>
2026-08-30 19:14:59 -0300

    added networkreceived and sent in the metric struct

--- internal/model/metric.go
+++ internal/model/metric.go
@@ -3,8 +3,10 @@ package model
 import "time"
 
 type Metric struct {
-	CPUUsage    float64   `json:"cpu_usage"`
-	MemoryUsage float64   `json:"memory_usage"`
-	DiskUsage   float64   `json:"disk_usage"`
-	Timestamp   time.Time `json:"timestamp"`
+	CPUUsage       float64   `json:"cpu_usage"`
+	MemoryUsage    float64   `json:"memory_usage"`
+	DiskUsage      float64   `json:"disk_usage"`
+	NetworkReceive uint64    `json:"network_receive"`
+	NetworkSent    uint64    `json:"network_sent"`
+	Timestamp      time.Time `json:"timestamp"`
 }
