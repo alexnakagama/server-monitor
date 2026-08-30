0ce77fbe3c9dc9883d938a4912d25cb11f813b93
alexnakagama <alexnakagama4@gmail.com>
2026-08-30 18:46:03 -0300

    finished helper function to read the stats of the disk

--- internal/agent/collector/disk.go
+++ internal/agent/collector/disk.go
@@ -1,10 +1,27 @@
 package collector
 
+import "syscall"
+
 type diskStats struct {
 	total uint64
 	free  uint64
 }
 
-func readDiskStats() (float64, error)
+func readDiskStats() (diskStats, error) {
+	var stat syscall.Statfs_t
+
+	err := syscall.Statfs("/", &stat)
+	if err != nil {
+		return diskStats{}, err
+	}
+
+	total := stat.Blocks * uint64(stat.Bsize)
+	free := stat.Bavail * uint64(stat.Bsize)
+
+	return diskStats{
+		total: total,
+		free:  free,
+	}, nil
+}
 
 func DiskUsage() (float64, error)
