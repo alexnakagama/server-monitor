f9b2b1da2fdd28aa74ab2527cfd0fafc14b82233
alexnakagama <alexnakagama4@gmail.com>
2026-08-30 19:21:57 -0300

    tested function collect metric on main

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -8,29 +8,10 @@ import (
 )
 
 func main() {
-	cpu, err := collector.CPUUsage()
+	metric, err := collector.CollectMetrics()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	memory, err := collector.MemoryUsage()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	disk, err := collector.DiskUsage()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	network, err := collector.NetworkUsage()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("CPU: %.2f%%\n", cpu)
-	fmt.Printf("Memory: %.2f%%\n", memory)
-	fmt.Printf("Disk: %.2f%%\n", disk)
-	fmt.Printf("Nw recieved: %d bytes\n", network.Received)
-	fmt.Printf("Nw sent: %d\n", network.Sent)
+	fmt.Printf("%+v\n", metric)
 }
