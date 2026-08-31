89abf4007b5f8da68b5c92cc662eed3d88057b67
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 14:48:05 -0300

    finished function to connect

--- internal/server/db/postgres.go
+++ internal/server/db/postgres.go
@@ -6,4 +6,16 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-func NewPostgres(ctx context.Context, url string) (*pgxpool.Pool, error) {}
+func NewPostgres(ctx context.Context, url string) (*pgxpool.Pool, error) {
+	db, err := pgxpool.New(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(ctx); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
