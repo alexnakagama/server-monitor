d5da67ba663bbf07920d658012e636769820831a
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 11:08:10 -0300

    added serverid in the metric struct

--- internal/model/metric.go
+++ internal/model/metric.go
@@ -3,6 +3,7 @@ package model
 import "time"
 
 type Metric struct {
+	ServerID       int       `json:"server_id"`
 	CPUUsage       float64   `json:"cpu_usage"`
 	MemoryUsage    float64   `json:"memory_usage"`
 	DiskUsage      float64   `json:"disk_usage"`
