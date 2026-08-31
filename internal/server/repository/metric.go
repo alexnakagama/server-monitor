c77ffdc395c949d533510dba43f9994396cf618f
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 11:32:03 -0300

    deleted

--- internal/server/repository/metric.go
+++ internal/server/repository/metric.go
@@ -1,9 +1,6 @@
 package repository
 
 import (
-	"context"
-
-	"github.com/alexnakagama/server-monitor/internal/model"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -15,7 +12,4 @@ func NewMetricRepository(db *pgxpool.Pool) *MetricRepository {
 	return &MetricRepository{
 		db: db,
 	}
-}
-
-func (r *MetricRepository) Create(ctx context.Context, metric model.Metric) error {
 }
