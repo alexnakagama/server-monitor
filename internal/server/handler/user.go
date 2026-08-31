71bb9d236f18deb7bc6ffbd98a966c1f50fd9ac3
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 14:32:22 -0300

    finished the handler to register a user

--- internal/server/handler/user.go
+++ internal/server/handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/alexnakagama/server-monitor/internal/server/service"
@@ -22,4 +23,25 @@ type RegisterRequest struct {
 	Password string `json:"password"`
 }
 
-func (h *UserHandler) HandleRegister(w http.ResponseWriter, r *http.Request) {}
+func (h *UserHandler) HandleRegister(w http.ResponseWriter, r *http.Request) {
+	var req RegisterRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	err = h.service.Register(
+		r.Context(),
+		req.Username,
+		req.Email,
+		req.Password,
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
