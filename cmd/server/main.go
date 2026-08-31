fd961d824d2b77267f6fb102398ce98646fb5dc1
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 14:51:55 -0300

    added godotenv to load env file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Println("no .env file found")
+	}
 }
