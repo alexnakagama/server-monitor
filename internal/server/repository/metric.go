acdde9746df7cc53c648a4b78abe2087757dcf1b
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 11:23:37 -0300

    added function to create

--- internal/server/repository/metric.go
+++ internal/server/repository/metric.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type MetricRepository struct {
 	db *pgxpool.Pool
@@ -10,4 +15,7 @@ func NewMetricRepository(db *pgxpool.Pool) *MetricRepository {
 	return &MetricRepository{
 		db: db,
 	}
+}
+
+func (r *MetricRepository) Create(ctx context.Context, metric model.Metric) error {
 }
