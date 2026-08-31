7d89f2d404b303709ca29738895428d831790bf0
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 12:52:18 -0300

    added register request for handler

--- internal/server/handler/user.go
+++ internal/server/handler/user.go
@@ -25,4 +25,10 @@ func NewUserHandler(repo UserRespository) *UserHandler {
 	}
 }
 
+type RegisterRequest struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
 func (h *UserHandler) HandleCreate(w http.ResponseWriter, r *http.Request) {}
