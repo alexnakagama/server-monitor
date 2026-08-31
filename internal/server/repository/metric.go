acaa60f959146cc4e314f2ea816b9907f9e664c4
alexnakagama <alexnakagama4@gmail.com>
2026-08-31 11:20:45 -0300

    created function to create a new metric repository

--- internal/server/repository/metric.go
+++ internal/server/repository/metric.go
@@ -5,3 +5,9 @@ import "github.com/jackc/pgx/v5/pgxpool"
 type MetricRepository struct {
 	db *pgxpool.Pool
 }
+
+func NewMetricRepository(db *pgxpool.Pool) *MetricRepository {
+	return &MetricRepository{
+		db: db,
+	}
+}
