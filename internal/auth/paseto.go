2e6f79c61bb9ffa9e90485846c9e1660243f70e6
alexnakagama <alexnakagama4@gmail.com>
2026-09-02 00:38:55 -0300

    finished function to verify a token

--- internal/auth/paseto.go
+++ internal/auth/paseto.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
 	"aidanwoods.dev/go-paseto"
@@ -27,4 +28,21 @@ func (p *PasetoManager) CreateToken(userID int) (string, error) {
 	return token.V4Encrypt(p.key, nil), nil
 }
 
-func (p *PasetoManager) VerifyToken(token string) (int, error) {}
+func (p *PasetoManager) VerifyToken(token string) (int, error) {
+	parsedToken, err := paseto.NewParser().ParseV4Local(p.key, token, nil)
+	if err != nil {
+		return 0, nil
+	}
+
+	userStringID, err := parsedToken.GetString("user_id")
+	if err != nil {
+		return 0, nil
+	}
+
+	userID, err := strconv.Atoi(userStringID)
+	if err != nil {
+		return 0, nil
+	}
+
+	return userID, nil
+}
