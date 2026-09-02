dd17198d9349dc7824347d000c30090bb19fc9dc
alexnakagama <alexnakagama4@gmail.com>
2026-09-02 00:02:44 -0300

    updated the register function on service to manage the error

--- internal/server/service/user.go
+++ internal/server/service/user.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 
 	"github.com/alexnakagama/server-monitor/internal/model"
+	apperrors "github.com/alexnakagama/server-monitor/internal/server/repository"
 )
 
 type UserRepository interface {
@@ -31,9 +32,17 @@ func (s *UserService) Register(ctx context.Context, username string, email strin
 		return errors.New("username already exists")
 	}
 
+	if !errors.Is(err, apperrors.ErrUserNotFound) {
+		return err
+	}
+
 	_, err = s.repository.GetByEmail(ctx, email)
 	if err != nil {
 		return errors.New("email already exists")
+	}
+
+	if !errors.Is(err, apperrors.ErrUserNotFound) {
+		return err
 	}
 
 	passwordHash, err := HashPassword(password)
