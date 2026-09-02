0fbe9a4014813486197a82791e07d0bedee8148c
alexnakagama <alexnakagama4@gmail.com>
2026-09-02 14:28:04 -0300

    finished function to get userid from context

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -6,4 +6,7 @@ type contextKey string
 
 const userIDKey contextKey = "userID"
 
-func UserIDFromContext(ctx context.Context) (int, bool) {}
+func UserIDFromContext(ctx context.Context) (int, bool) {
+	userID, ok := ctx.Value(userIDKey).(int)
+	return userID, ok
+}
