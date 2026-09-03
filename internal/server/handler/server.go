987275ea65e6e6326cc06c72148646ff40624e9c
alexnakagama <alexnakagama4@gmail.com>
2026-09-03 00:39:54 -0300

    added function to create the handler

--- internal/server/handler/server.go
+++ internal/server/handler/server.go
@@ -5,3 +5,5 @@ import "github.com/alexnakagama/server-monitor/internal/server/service"
 type ServerHandler struct {
 	service *service.ServerService
 }
+
+func NewServerHandler(service *service.ServerService) *ServerHandler {}
