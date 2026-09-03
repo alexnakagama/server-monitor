a6ee7d0ff8f6479d6503698e79f1d61d45f57be5
alexnakagama <alexnakagama4@gmail.com>
2026-09-03 00:32:52 -0300

    added interface for the server repo in the server service

--- internal/server/service/server.go
+++ internal/server/service/server.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"context"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
+)
+
+type ServerRepository interface {
+	Create(ctx context.Context, server model.Server) error
+}
