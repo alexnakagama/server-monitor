8031c62cb77e0b6a136746e6e37468416ce45a31
alexnakagama <alexnakagama4@gmail.com>
2026-09-03 19:38:32 -0300

    deleted getbyhostname now it will return just one

--- internal/server/repository/server.go
+++ internal/server/repository/server.go
@@ -143,42 +143,10 @@ func (r *ServerRepository) GetByOS(ctx context.Context, os string) ([]model.Serv
 	return servers, nil
 }
 
-func (r *ServerRepository) GetByHostname(ctx context.Context, hostname string) ([]model.Server, error) {
+func (r *ServerRepository) GetByHostname(ctx context.Context, hostname string) (model.Server, error) {
 	query := `
 		SELECT id, name, hostname, os, created_at
 		FROM servers
 		WHERE hostname = $1
 	`
-
-	rows, err := r.db.Query(ctx, query, hostname)
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var servers []model.Server
-
-	for rows.Next() {
-		var server model.Server
-
-		err := rows.Scan(
-			&server.ID,
-			&server.Name,
-			&server.Hostname,
-			&server.OS,
-			&server.CreatedAt,
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		servers = append(servers, server)
-	}
-
-	err = rows.Err()
-	if err != nil {
-		return nil, err
-	}
-
-	return servers, nil
 }
