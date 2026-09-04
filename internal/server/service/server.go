520a1d8da89062ff9a975e91c5fad15639b5674d
alexnakagama <alexnakagama4@gmail.com>
2026-09-04 17:46:05 -0300

    added the validate to updatenamebyhostname

--- internal/server/service/server.go
+++ internal/server/service/server.go
@@ -93,7 +93,12 @@ func (s *ServerService) UpdateByHostname(ctx context.Context, hostname string, n
 }
 
 func (s *ServerService) UpdateNameByHostname(ctx context.Context, name string, hostname string) error {
-	err := model.ValidateName(name)
+	err := model.ValidateHostname(hostname)
+	if err != nil {
+		return err
+	}
+
+	err = model.ValidateName(name)
 	if err != nil {
 		return err
 	}
