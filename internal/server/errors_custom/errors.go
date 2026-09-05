2f704fab5fde95c5625436cdd43d7206f9fb29aa
alexnakagama <alexnakagama4@gmail.com>
2026-09-05 15:13:36 -0300

    added another error

--- internal/server/errors_custom/errors.go
+++ internal/server/errors_custom/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 var (
 	ErrUsernameRequired = errors.New("username is required")
+	ErrEmailRequired    = errors.New("email is required")
 
 	ErrServerNotFound = errors.New("server not found")
 	ErrUserNotFound   = errors.New("user not found")
