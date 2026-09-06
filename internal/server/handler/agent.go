624c6c87c2998a2fce8d8972962ed79f63a0db81
alexnakagama <alexnakagama4@gmail.com>
2026-09-06 01:01:44 -0300

    added dto for a request to create an agent

--- internal/server/handler/agent.go
+++ internal/server/handler/agent.go
@@ -16,4 +16,7 @@ func NewAgentHandler(service *service.AgentService) *AgentHandler {
 	}
 }
 
+type CreateAgentRequest struct {
+}
+
 func (h *AgentHandler) HandleCreate(w *http.ResponseWriter, r *http.Request) {}
