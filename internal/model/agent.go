fc794c1facd28b1c10468ed4c87d019d61d786be
alexnakagama <alexnakagama4@gmail.com>
2026-09-05 23:37:38 -0300

    finished the agent model

--- internal/model/agent.go
+++ internal/model/agent.go
@@ -1,1 +1,11 @@
 package model
+
+import "time"
+
+type Agent struct {
+	ID        int       `json:"id"`
+	ServerID  int       `json:"server_id"`
+	Name      string    `json:"name"`
+	TokenHash string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
