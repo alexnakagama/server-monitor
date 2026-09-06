2d36924242c8e9bbba540301c237e7bc66c1706f
alexnakagama <alexnakagama4@gmail.com>
2026-09-06 00:47:11 -0300

    deleted unused dependencie

--- internal/server/service/agent.go
+++ internal/server/service/agent.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"errors"
 
 	"github.com/alexnakagama/server-monitor/internal/model"
 	"github.com/alexnakagama/server-monitor/internal/server/errors_custom"
