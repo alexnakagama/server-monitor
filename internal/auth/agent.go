3eab96422a550a73e82e8322ad6ea99750a429bf
alexnakagama <alexnakagama4@gmail.com>
2026-09-06 20:00:10 -0300

    finised the auth agent middleware

--- internal/auth/agent.go
+++ internal/auth/agent.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/alexnakagama/server-monitor/internal/model"
 )
@@ -11,4 +12,41 @@ type AgentAuthenticator interface {
 	Authenticate(ctx context.Context, token string) (model.Agent, error)
 }
 
-func AgentAuthMiddleware(agentAuthenticator AgentAuthenticator, next http.Handler) http.Handler {}
+type agentIDKey string
+type serverIDKey string
+
+const (
+	agentIDContextKey  agentIDKey  = "agentID"
+	serverIDContextKey serverIDKey = "serverID"
+)
+
+func AgentAuthMiddleware(agentAuthenticator AgentAuthenticator, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHandler := r.Header.Get("Authorization")
+
+		if authHandler == "" {
+			http.Error(w, "missing authorization header", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.SplitN(authHandler, " ", 2)
+
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "invalid authotization header", http.StatusUnauthorized)
+			return
+		}
+
+		token := parts[1]
+
+		agent, err := agentAuthenticator.Authenticate(r.Context(), token)
+		if err != nil {
+			http.Error(w, "invalid agent token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), agentIDContextKey, agent.ID)
+		ctx = context.WithValue(r.Context(), serverIDContextKey, agent.ServerID)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
