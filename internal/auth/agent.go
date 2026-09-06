3c99e7b97afc383e8df1cf9ad1db4638b5afe218
alexnakagama <alexnakagama4@gmail.com>
2026-09-06 19:51:41 -0300

    added interface for the agent service

--- internal/auth/agent.go
+++ internal/auth/agent.go
@@ -1,1 +1,11 @@
 package auth
+
+import (
+	"context"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
+)
+
+type AgentAuthenticator interface {
+	Authenticate(ctx context.Context, token string) (model.Agent, error)
+}
