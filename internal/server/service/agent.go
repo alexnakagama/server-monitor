7a1b61d958b9c65a005c03461c1090e460929b2f
alexnakagama <alexnakagama4@gmail.com>
2026-09-06 00:00:00 -0300

    added create method to the interface agentrepo

--- internal/server/service/agent.go
+++ internal/server/service/agent.go
@@ -1,4 +1,11 @@
 package service
 
+import (
+	"context"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
+)
+
 type AgentRepository interface {
+	Create(ctx context.Context, agent model.Agent) error
 }
