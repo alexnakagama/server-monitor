bea4fa72ae88f79729d8edfc2af727ff9d5e5145
alexnakagama <alexnakagama4@gmail.com>
2026-09-06 19:13:18 -0300

    finished the handler to create an agent

--- internal/server/handler/agent.go
+++ internal/server/handler/agent.go
@@ -1,7 +1,9 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/alexnakagama/server-monitor/internal/server/service"
 )
@@ -20,4 +22,38 @@ type CreateAgentRequest struct {
 	Name string `json:"name"`
 }
 
-func (h *AgentHandler) HandleCreate(w *http.ResponseWriter, r *http.Request) {}
+func (h *AgentHandler) HandleCreate(w http.ResponseWriter, r *http.Request) {
+	serverID, err := strconv.Atoi(r.PathValue("serverID"))
+	if err != nil {
+		http.Error(w, "invalid server id", http.StatusBadRequest)
+		return
+	}
+
+	var req CreateAgentRequest
+
+	err = json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	token, err := h.service.Create(r.Context(), serverID, req.Name)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := struct {
+		Token string `json:"token"`
+	}{
+		Token: token,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		return
+	}
+}
