3a7060808aa31ba2eed79189883d92e60e27d662
alexnakagama <alexnakagama4@gmail.com>
2026-09-06 00:05:57 -0300

    added struct for agentservice with the agentrepo interfacew

--- internal/server/service/agent.go
+++ internal/server/service/agent.go
@@ -10,3 +10,7 @@ type AgentRepository interface {
 	Create(ctx context.Context, agent model.Agent) error
 	GetByID(ctx context.Context, agentID int) (model.Agent, error)
 }
+
+type AgentService struct {
+	repository AgentRepository
+}
