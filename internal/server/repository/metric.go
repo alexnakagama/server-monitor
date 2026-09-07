e8c87d6689445096564938bdf725378bcdf0b539
alexnakagama <alexnakagama4@gmail.com>
2026-09-07 16:03:04 -0300

    finished the function create on the metric repository

--- internal/server/repository/metric.go
+++ internal/server/repository/metric.go
@@ -17,4 +17,29 @@ func NewMetricRepository(db *pgxpool.Pool) *MetricRepository {
 	}
 }
 
-func (r *MetricRepository) Create(ctx context.Context, metric model.Metric) error {}
+func (r *MetricRepository) Create(ctx context.Context, metric model.Metric) error {
+	query := `
+		INSERT INTO server_metrics (
+			server_id,
+			cpu_usage,
+			memory_usage,
+			disk_usage,
+			network_receive,
+			network_sent
+		)
+		VALUES ($1, $2, $3, $4, $5, $6)
+	`
+
+	_, err := r.db.Exec(
+		ctx,
+		query,
+		metric.ServerID,
+		metric.CPUUsage,
+		metric.MemoryUsage,
+		metric.DiskUsage,
+		metric.NetworkReceive,
+		metric.NetworkSent,
+	)
+
+	return err
+}
