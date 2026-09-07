f1214f9f7b8f91649cb37b77899d806ece1dc9c4
alexnakagama <alexnakagama4@gmail.com>
2026-09-07 16:12:06 -0300

    created the struct for a metric service

--- internal/server/service/metric.go
+++ internal/server/service/metric.go
@@ -10,3 +10,7 @@ type MetricRepository interface {
 	Create(ctx context.Context, metric model.Metric) error
 	GetByID(ctx context.Context, metricID int) (model.Metric, error)
 }
+
+type MetricService struct {
+	repository MetricRepository
+}
