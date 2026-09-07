eb12175ecc59cb8eec3885aa434668ddc899a7be
alexnakagama <alexnakagama4@gmail.com>
2026-09-07 16:10:53 -0300

    created the metric repo interface on the service

--- internal/server/service/metric.go
+++ internal/server/service/metric.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"context"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
+)
+
+type MetricRepository interface {
+	Create(ctx context.Context, metric model.Metric) error
+}
