3711d12f5096ed8ca6021e1214a848c8a538d1da
alexnakagama <alexnakagama4@gmail.com>
2026-09-07 16:40:07 -0300

    created constructor for metric hander

--- internal/server/handler/metric.go
+++ internal/server/handler/metric.go
@@ -5,3 +5,5 @@ import "github.com/alexnakagama/server-monitor/internal/server/service"
 type MetricHandler struct {
 	service *service.ServerService
 }
+
+func NewMetricHandler(service *service.ServerService) *MetricHandler {}
