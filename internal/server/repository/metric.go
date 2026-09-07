1fbadfb7a7ef45b2f1ea28af99a34ba66c60c0bc
alexnakagama <alexnakagama4@gmail.com>
2026-09-07 15:50:49 -0300

    added the firm of the method create on the metric repository

--- internal/server/repository/metric.go
+++ internal/server/repository/metric.go
@@ -1,6 +1,9 @@
 package repository
 
 import (
+	"context"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -13,3 +16,5 @@ func NewMetricRepository(db *pgxpool.Pool) *MetricRepository {
 		db: db,
 	}
 }
+
+func (r *MetricRepository) Create(ctx context.Context, metric model.Metric) error {}
