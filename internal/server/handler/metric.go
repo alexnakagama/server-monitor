e697626bb850fb21b5b2a4fba2098b1651100b0d
alexnakagama <alexnakagama4@gmail.com>
2026-09-07 23:29:56 -0300

    added struct to represent the request to create a metric

--- internal/server/handler/metric.go
+++ internal/server/handler/metric.go
@@ -16,4 +16,13 @@ func NewMetricHandler(service *service.MetricService) *MetricHandler {
 	}
 }
 
-func (h *MetricHandler) HandleCreate(w http.ResponseWriter, r *http.Request) {}
+type CreateMetricRequest struct {
+	CPUUsage       float64 `json:"cpu_usage"`
+	MemoryUsage    float64 `json:"memory_usage"`
+	DiskUsage      float64 `json:"disk_usage"`
+	NetworkReceive uint64  `json:"network_receive"`
+	NetworkSent    uint64  `json:"network_sent"`
+}
+
+func (h *MetricHandler) HandleCreate(w http.ResponseWriter, r *http.Request) {
+}
