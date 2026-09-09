a9f9448f9c0181e1d900e39a3d58d9462862f057
alexnakagama <alexnakagama4@gmail.com>
2026-09-09 14:15:21 -0300

    loading the env file with godotenv

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"log"
 	"os"
 	"time"
 
 	"github.com/alexnakagama/server-monitor/internal/agent"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("no .env file found")
+	}
+
 	apiURL := os.Getenv("API_URL")
 	token := os.Getenv("AGENT_TOKEN")
 
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load("../../.env")
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println("no .env file found")
 	}
