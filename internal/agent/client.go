5535c9afe9908499097159c321ab5da3b0a96735
alexnakagama <alexnakagama4@gmail.com>
2026-09-09 00:52:16 -0300

    fnished the method to send metric

--- internal/agent/client.go
+++ internal/agent/client.go
@@ -1,6 +1,9 @@
 package agent
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/alexnakagama/server-monitor/internal/model"
@@ -20,4 +23,35 @@ func NewClient(baseURL, token string) *Client {
 	}
 }
 
-func (c *Client) SendMetric(metric model.Metric) error {}
+func (c *Client) SendMetric(metric model.Metric) error {
+	body, err := json.Marshal(metric)
+	if err != nil {
+		return err
+	}
+
+	url := c.baseURL + "/agents/metrics"
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		url,
+		bytes.NewReader(body),
+	)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+c.token)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
+}
