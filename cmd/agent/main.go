6408bc1d6a9182e78f0082661800fcb2126a2e59
alexnakagama <alexnakagama4@gmail.com>
2026-09-09 14:12:23 -0300

    getting the secrets from env file using os

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"github.com/alexnakagama/server-monitor/internal/agent"
 )
 
 func main() {
-	client := agent.NewClient(
-		"http://localhost:8080",
-		"TU_AGENT_TOKEN",
-	)
+	apiURL := os.Getenv("API_URL")
+	token := os.Getenv("AGENT_TOKEN")
+
+	client := agent.NewClient(apiURL, token)
 
 	agent := agent.New(100*time.Second, client)
 	agent.Run()
