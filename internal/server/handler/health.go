ff1cb1fb868eb9a4e1c85e355bee27076a5d5325
alexnakagama <alexnakagama4@gmail.com>
2026-09-10 21:23:34 -0300

    finished the handler

--- internal/server/handler/health.go
+++ internal/server/handler/health.go
@@ -1,6 +1,9 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type HealthHandler struct{}
 
@@ -8,4 +11,18 @@ func NewHealthHandler() *HealthHandler {
 	return &HealthHandler{}
 }
 
-func (h *HealthHandler) Handle(w http.ResponseWriter, r *http.Request) {}
+func (h *HealthHandler) Handle(w http.ResponseWriter, r *http.Request) {
+	response := struct {
+		Status string `json:"status"`
+	}{
+		Status: "ok",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err := json.NewEncoder(w).Encode(response)
+	if err != nil {
+		return
+	}
+}
