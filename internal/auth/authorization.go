78ae67b25978b50b96116f89600e9318de3a9221
alexnakagama <alexnakagama4@gmail.com>
2026-09-11 16:08:37 -0300

    finished the requirerole function

--- internal/auth/authorization.go
+++ internal/auth/authorization.go
@@ -2,4 +2,19 @@ package auth
 
 import "net/http"
 
-func RequireRole(role string, next http.Handler) http.Handler {}
+func RequireRole(role string, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		userRole, ok := RoleFromContext(r.Context())
+		if !ok {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		if userRole != role {
+			http.Error(w, "forbidden", http.StatusForbidden)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
