c97cac264aabbf45f0318f6eab0c947381147fdf
alexnakagama <alexnakagama4@gmail.com>
2026-09-12 22:51:43 -0300

    finished load function

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,27 @@
 package config
 
+import (
+	"os"
+	"strconv"
+)
+
 type Config struct {
 	DatabaseURL         string
 	PasetoKey           string
 	MetricRetentionDays int
 }
 
-func Load() (Config, error) {}
+func Load() (Config, error) {
+	databaseURL := os.Getenv("DATABASE_URL")
+	pasetoKey := os.Getenv("PASETO_KEY")
+	retentionDays, err := strconv.Atoi(os.Getenv("METRIC_RETENTION_DAYS"))
+	if err != nil {
+		return Config{}, err
+	}
+
+	return Config{
+		DatabaseURL:         databaseURL,
+		PasetoKey:           pasetoKey,
+		MetricRetentionDays: retentionDays,
+	}, nil
+}
