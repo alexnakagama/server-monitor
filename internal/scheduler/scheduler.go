78e6ad52906295c2f789ee11ed24cf06c2e3e5cc
alexnakagama <alexnakagama4@gmail.com>
2026-09-12 23:17:36 -0300

    finished the run function and added comments explaining it

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -1,6 +1,10 @@
 package scheduler
 
-import "context"
+import (
+	"context"
+	"log"
+	"time"
+)
 
 type metricService interface {
 	DeleteOldMetrics(ctx context.Context) error
@@ -17,4 +21,25 @@ func NewScheduler(metricService metricService) *Scheduler {
 }
 
 func (s *Scheduler) Run(ctx context.Context) {
+	// creating a ticker
+	// one of the campos its a channel
+	// one time for each 24 hours it sends a value through that channel
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
+
+	for {
+		// select stays blocked waiting for one of the channels to execute
+		select {
+		// receives a value of the ticker.c channel
+		// in this case we arent interested in the value but the moment of the ticker
+		case <-ticker.C:
+			err := s.metricService.DeleteOldMetrics(ctx)
+			if err != nil {
+				log.Printf("metric cleanup error: %v", err)
+			}
+
+		case <-ctx.Done():
+			return
+		}
+	}
 }
