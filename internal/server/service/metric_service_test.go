9c90b7d816905acc21e4a5c287c7c47bab94c0a6
alexnakagama <alexnakagama4@gmail.com>
2026-09-13 18:52:02 -0300

    created for the metric service tests a mock of the repository

--- internal/server/service/metric_service_test.go
+++ internal/server/service/metric_service_test.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
+)
+
+type MetricRepositoryMock struct {
+	create          func(ctx context.Context, metric model.Metric) error
+	getByID         func(ctx context.Context, metricID int) (model.Metric, error)
+	getByServerID   func(ctx context.Context, serverID int, filters model.MetricFilters) ([]model.Metric, error)
+	deleteOlderThan func(ctx context.Context, before time.Time) error
+}
