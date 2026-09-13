1bc49a722d7f662285780ebe8eb2a8d3ee9554e1
alexnakagama <alexnakagama4@gmail.com>
2026-09-13 19:28:31 -0300

    added all the methods to the mock

--- internal/server/service/agent_service_test.go
+++ internal/server/service/agent_service_test.go
@@ -1,6 +1,16 @@
 package service
 
+import (
+	"context"
+
+	"github.com/alexnakagama/server-monitor/internal/model"
+)
+
 var _ AgentRepository = (*AgentRepositoryMock)(nil)
 
 type AgentRepositoryMock struct {
+	create         func(ctx context.Context, agent model.Agent) error
+	getByID        func(ctx context.Context, agentID int) (model.Agent, error)
+	deleteByID     func(ctx context.Context, agentID int) error
+	getByTokenHash func(ctx context.Context, tokenHash string) (model.Agent, error)
 }
