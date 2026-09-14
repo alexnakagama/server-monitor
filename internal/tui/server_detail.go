9295c251c46dc3fcd762ed9c3bcf48d4cc6c69d0
alexnakagama <alexnakagama4@gmail.com>
2026-09-14 12:59:00 -0300

    created the model for the server detail

--- internal/tui/server_detail.go
+++ internal/tui/server_detail.go
@@ -1,1 +1,11 @@
 package tui
+
+import (
+	"github.com/alexnakagama/server-monitor/internal/model"
+	"github.com/alexnakagama/server-monitor/internal/monitor"
+)
+
+type ServerDetailModel struct {
+	client *monitor.Client
+	server model.Server
+}
