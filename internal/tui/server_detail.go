a269ff5c4a5ce3514f19f303f65104a23a8d3632
alexnakagama <alexnakagama4@gmail.com>
2026-09-14 13:02:58 -0300

    added upodate and view functions

--- internal/tui/server_detail.go
+++ internal/tui/server_detail.go
@@ -22,4 +22,27 @@ func (m *ServerDetailModel) Init() tea.Cmd {
 	return nil
 }
 
-func (m *ServerDetailModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {}
+func (m *ServerDetailModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, quit()
+		}
+	}
+
+	return m, nil
+}
+
+func (m *ServerDetailModel) View() string {
+	view := "Server Monitor\n\n"
+	view += "Server Details\n\n"
+
+	view += "Name: " + m.server.Name + "\n"
+	view += "Hostname: " + m.server.Hostname + "\n"
+	view += "OS: " + m.server.OS + "\n"
+
+	view += "\nPress q to quit\n"
+
+	return view
+}
