71de6597bc4e9e7ab2c29b904e225ab6fde8f038
alexnakagama <alexnakagama4@gmail.com>
2026-09-14 11:30:03 -0300

    finished the login function

--- internal/monitor/client.go
+++ internal/monitor/client.go
@@ -1,10 +1,17 @@
 package monitor
 
-import "context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Client struct {
 	baseURL string
 	token   string
+	client  *http.Client
 }
 
 func NewClient(baseURL string) *Client {
@@ -22,4 +29,49 @@ type loginResponse struct {
 	Token string `json:"token"`
 }
 
-func (c *Client) Login(ctx context.Context, username string, password string) error {}
+func (c *Client) Login(ctx context.Context, username string, password string) error {
+	reqBody := loginRequest{
+		Username: username,
+		Password: password,
+	}
+
+	body, err := json.Marshal(reqBody)
+	if err != nil {
+		return err
+	}
+
+	url := c.baseURL + "/login"
+
+	req, err := http.NewRequestWithContext(
+		ctx,
+		http.MethodPost,
+		url,
+		bytes.NewReader(body),
+	)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("login failed with status code: %d", resp.StatusCode)
+	}
+
+	var loginResp loginResponse
+
+	err = json.NewDecoder(resp.Body).Decode(&loginResp)
+	if err != nil {
+		return err
+	}
+
+	c.token = loginResp.Token
+
+	return nil
+}
