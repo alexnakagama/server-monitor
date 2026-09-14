61fa3d1c624e1e784f8ead3d62dcd10da84e2409
alexnakagama <alexnakagama4@gmail.com>
2026-09-14 11:41:15 -0300

    finished function to create a login model

--- internal/tui/login.go
+++ internal/tui/login.go
@@ -6,3 +6,18 @@ type LoginModel struct {
 	username textinput.Model
 	password textinput.Model
 }
+
+func NewLoginModel() LoginModel {
+	username := textinput.New()
+	username.Placeholder = "username"
+	username.Focus()
+
+	password := textinput.New()
+	password.Placeholder = "username"
+	password.Focus()
+
+	return LoginModel{
+		username: username,
+		password: password,
+	}
+}
