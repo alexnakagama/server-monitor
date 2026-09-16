36bd5bf16e55cb953ae2925d1687ce0c39670df6
alexnakagama <alexnakagama4@gmail.com>
2026-09-16 16:08:34 -0300

    created the view update and init methods

--- internal/tui/app.go
+++ internal/tui/app.go
@@ -13,3 +13,27 @@ func NewAppModel(screen tea.Model) AppModel {
 		screen: screen,
 	}
 }
+
+func (m *AppModel) Init() tea.Cmd {
+	return m.screen.Init()
+}
+
+func (m *AppModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+
+		return m, nil
+	}
+
+	var cmd tea.Cmd
+
+	m.screen, cmd = m.screen.Update(msg)
+
+	return m, cmd
+}
+
+func (m *AppModel) View() string {
+	return m.screen.View()
+}
