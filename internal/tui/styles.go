b98ace24525cf7c249a3e6e97fbd184c9cf92a80
alexnakagama <alexnakagama4@gmail.com>
2026-09-16 16:32:45 -0300

    added new style

--- internal/tui/styles.go
+++ internal/tui/styles.go
@@ -38,3 +38,8 @@ var osColumnStyle = lipgloss.NewStyle().
 
 var prefixStyle = lipgloss.NewStyle().
 	Width(2)
+
+var sectionBoxStyle = lipgloss.NewStyle().
+	Border(lipgloss.RoundedBorder()).
+	Padding(0, 2).
+	MarginBottom(1)
